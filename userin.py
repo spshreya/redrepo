9d7ac33926616887266b61ad9201c79c067a1c89
spshreya <shreya.pandey.emip@gmail.com>
2019-06-13 07:17:09 +0000

    my first python code on github

---
+++ userin.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+                                                                                                                                                     
+import datetime
+name = input('Enter your name:')
+age = int(input('Enter your age:'))
+                                                                                                                                                     
+nage=2019-age
+req=nage+95
+#now=datetime.datetime.now()
+                                                                                                                                                     
+print('The year you will turn 95 is:',req)
