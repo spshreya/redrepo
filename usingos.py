d1b09135b56570ef352a4f82e536f5cfb4d38886
spshreya <shreya.pandey.emip@gmail.com>
2019-06-18 04:44:16 +0000

    learning python

--- usingos.py
+++ usingos.py
@@ -1,10 +1,9 @@
 import os
                                                                                                                                                      
-x=input("enter string:")
-                                                                                                                                                     
-y=type(x)
-                                                                                                                                                     
-if y=='<class \'str\'>':
+x=raw_input("enter string:")
+print(type(x))                                                                                                                                                                                                                                                                                            
+if x==str(x):
         os.system("useradd Shreya")
+	os.system("passwd Shreya")
 else:
         print("you did not enter a string")
